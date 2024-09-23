25a8ba7b479d16428ea8449e077f3229d1042655
ponkiss <sm719112005@gmail.com>
2024-09-22 18:50:54 -0600

    Update flight_reservations.sql

--- flight_reservations.sql
+++ flight_reservations.sql
@@ -1,31 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 21, 2024 at 02:49 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `flight_reservations`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `flights`
---
 
 CREATE TABLE `flights` (
   `id` int(11) NOT NULL,
@@ -38,20 +14,11 @@ CREATE TABLE `flights` (
   `is_reserved` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `flights`
---
-
 INSERT INTO `flights` (`id`, `flight_number`, `origin`, `destination`, `departure_time`, `arrival_time`, `price`, `is_reserved`) VALUES
-(1, '1', 'Ciudad Juarez', 'Washington, D. C', '2024-09-20 19:20:00', '2024-09-20 08:20:00', 3500.00, 0),
-(3, '3', 'El Paso, Texas', 'Buenos Aires', '2024-09-30 16:12:00', '2024-10-01 08:12:00', 5000.00, 1),
-(5, '4', 'Manitoba', 'Paris', '2024-10-17 06:59:00', '2024-10-11 08:56:00', 3000.00, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reservations`
---
+(1, '1', 'Ciudad Juarez', 'Cartagena', '2024-09-20 19:20:00', '2024-09-20 08:20:00', 3500.00, 0),
+(5, '4', 'Manitoba', 'Paris', '2024-10-17 06:59:00', '2024-10-11 08:56:00', 3000.00, 1),
+(10, '10', 'Pekin', 'Singapur', '2024-09-21 16:45:00', '2024-09-21 16:45:00', 1000.00, 0),
+(11, '42', 'Chile', 'Afghanistan', '2024-09-21 17:01:00', '2025-03-12 05:01:00', 10.00, 0);
 
 CREATE TABLE `reservations` (
   `id` int(11) NOT NULL,
@@ -60,18 +27,8 @@ CREATE TABLE `reservations` (
   `reservation_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `reservations`
---
-
 INSERT INTO `reservations` (`id`, `user_id`, `flight_id`, `reservation_date`) VALUES
-(1, 2, 1, '2024-09-21 00:20:24');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
+(20, 2, 1, '2024-09-21 23:20:03');
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -81,77 +38,40 @@ CREATE TABLE `users` (
   `role` enum('user','admin') DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
-
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `role`) VALUES
 (1, 'admin', '$2y$10$oBvxq7//liMWK6TlhsXqVOAgglel1KlLiJRlQSc3f4T4LRbPUICte', 'admin@gmail.com', 'admin'),
 (2, 'user', '$2y$10$H6baoWq64ZoxZC1GKowH.u8MK2NGnuxyvTfJC9lwEah5qk/loxrGm', 'user@gmail.com', 'user'),
 (3, 'zamora', '$2y$10$6ALeQ.bzxsd26RuiXXq14ufVIJE2YoTaoi9R8gHphi0DHnPHBY5Mi', 'zamora@gmail.com', 'user'),
 (4, 'karin', '$2y$10$mH38zJE7QMOdny3103hMAeLTGCcdHsZcj.etzJGLTKFduIbjjMVWm', 'karin@gmail.com', 'user'),
-(5, 'caleb', '$2y$10$/cKGl8Lp381P4BQZX3oUlu0kVOf57oZoqzCcMl9XC8Aclrvi33hSm', 'caleb@gmail.com', 'user');
+(5, 'caleb', '$2y$10$/cKGl8Lp381P4BQZX3oUlu0kVOf57oZoqzCcMl9XC8Aclrvi33hSm', 'caleb@gmail.com', 'user'),
+(6, 'fer', '$2y$10$Y2uIzCti19nzLiDxbS2y9urOfwbxH5xczqXMaVBXqy.SY4WLiEWam', 'fer@gmail.com', 'user'),
+(7, 'user1', '$2y$10$SWyWMC206fha06x8KNS1DexWR/e/nr955NEpffkzNaUjSsY0DunFG', 'user1@gmail.com', 'user'),
+(8, 'user2', '$2y$10$MyN2DnPLwzplqgxUQZYxJ.vtecnALtieoQoDEID6zZBuVaS8P0Vw2', 'user2@gmail.com', 'user');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `flights`
---
 ALTER TABLE `flights`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `reservations`
---
 ALTER TABLE `reservations`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `flight_id` (`flight_id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `flights`
---
 ALTER TABLE `flights`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
---
--- AUTO_INCREMENT for table `reservations`
---
 ALTER TABLE `reservations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `reservations`
---
 ALTER TABLE `reservations`
   ADD CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`flight_id`) REFERENCES `flights` (`id`);
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
